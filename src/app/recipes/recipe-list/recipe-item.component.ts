da0a0d4b0f075139ee38205506bcc0667e54d536
LeoTM <leo.m@hotmail.co.uk>
2016-11-19 21:38:53 +0000

    Generate component (recipe item)

---
+++ src/app/recipes/recipe-list/recipe-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rb-recipe-item',
+  templateUrl: './recipe-item.component.html',
+  styles: []
+})
+export class RecipeItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
