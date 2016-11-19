542822e8439e94407eacdb702b7ea03ca69a0f81
LeoTM <leo.m@hotmail.co.uk>
2016-11-19 23:10:25 +0000

    Create recipe item class

--- src/app/recipes/recipe-list/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Recipe } from '../recipe';
 
 @Component({
   selector: 'rb-recipe-item',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class RecipeItemComponent implements OnInit {
+  @Input() recipe: Recipe;
+  recipeId: number;
 
   constructor() { }
 
