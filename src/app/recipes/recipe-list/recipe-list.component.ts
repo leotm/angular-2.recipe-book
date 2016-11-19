209bc35ac82d9ba8ccb12fe630deb9b3a21b95b3
LeoTM <leo.m@hotmail.co.uk>
2016-11-19 23:11:36 +0000

    Create recipe list class (with example)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe';
+
 @Component({
   selector: 'rb-recipe-list',
   templateUrl: './recipe-list.component.html',
   styles: []
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[];
+  recipe = new Recipe('Dummy', 'Description of Dummy', 'http://3manfactory.co.uk/wp-content/uploads/2012/12/3mf-dummy.jpg')
 
   constructor() { }
 
