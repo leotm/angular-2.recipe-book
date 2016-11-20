b5f5dca3c751fd2db593d7f7ad7c5002d24f4350
LeoTM <leo.m@hotmail.co.uk>
2016-11-20 01:40:09 +0000

    Define recipes class

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from './recipe';
+
 @Component({
   selector: 'rb-recipes',
   templateUrl: './recipes.component.html',
   styles: []
 })
 export class RecipesComponent implements OnInit {
+
+  selectedRecipe: Recipe;
 
   constructor() { }
 
