b595d165bc48bf08893f7849fafa14aae93288ab
LeoTM <leo.m@hotmail.co.uk>
2016-11-27 22:22:50 +0000

    Create shopping list service
    So that can later add recipe ingredients to shopping list.

--- src/app/recipes/shopping-list/shopping-list.component.ts
+++ src/app/recipes/shopping-list/shopping-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { Ingredient } from '../ingredient';
 
+import { ShoppingListService } from './shopping-list.service';
+
 @Component({
   selector: 'rb-shopping-list',
   templateUrl: './shopping-list.component.html',
@@ -11,9 +13,10 @@ export class ShoppingListComponent implements OnInit {
 
   items: Ingredient[] = [];
 
-  constructor() { }
+  constructor(private sls: ShoppingListService) { }
 
   ngOnInit() {
+    this.items = this.sls.getItems();
   }
 
 }
